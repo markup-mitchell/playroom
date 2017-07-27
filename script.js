1ffec61ce447aaf6faf01e8df7438ff62abe635c
spitchell <mr.spitchell@gmail.com>
2017-07-27 12:55:46 +0100

    animation functions added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// alert("js is working");
+
+// All elements that animate //
+const IDs = ['lbP', 'lbA', 'lbY', 'lbM', 'lbPrightwing', 'lbPleftwing', 'lbArightwing', 'lbAleftwing',  'lbYrightwing', 'lbYleftwing', 'lbMrightwing', 'lbMleftwing'];
+
+const LADYBIRD = ['P','A', 'Y', 'M'];
+
+// auto-clear animation classes on completion //
+IDs.forEach(function(id) { 
+   let element = document.querySelector(`#${id}`);
+   element.addEventListener('animationend', function(){element.classList.remove('rightFlap', 'leftFlap', 'spin')})
+})
+
+
+function makeFlap(letter) {
+   let rightwing = document.querySelector(`#lb${letter}rightwing`);
+   let leftwing = document.querySelector(`#lb${letter}leftwing`);
+   rightwing.classList.add('rightFlap');
+   leftwing.classList.add('leftFlap');
+}
+
+function spin(letter) {
+   let ladybird = document.querySelector(`#lb${letter}`);
+   ladybird.classList.add('spin');
+}
+
+function random(array) {
+   return array[Math.floor(Math.random() * array.length)];
+}
+
+// window.setInterval(makeFlap(random(LADYBIRD), 2000);
+
+
+   
